9109cc7a989012edb3ae090c5d428b2a71973aca
CORETEXA\JamesGeary <james.geary@c3posttrade.com>
2018-11-29 11:18:46 +1300

    initialcommit

---
+++ FailsToWeave/SimpleClass.cs
@@ -0,0 +1,26 @@
+﻿using FailsToWeave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: WeaveAllMethodsInAssemblyAttribute()]
+
+namespace FailsToWeave
+{
+    public class SimpleClass
+    {
+        public delegate void ThreadFinishedEventHandler();
+
+        public struct MyStruct
+        {
+            public string MyString { get; set; }
+        }
+
+        public void DoSomething()
+        {
+            int myval = 5 + 2;
+        }
+    }
+}
